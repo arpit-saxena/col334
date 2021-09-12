5a11acc12cc903174803a9fbf38fc0088c707475
Arpit Saxena <arpit.saxena2000@yahoo.in>
2021-09-12 11:58:18 +0530

    Add CMake build setup

---
+++ a2/src/client.cpp
@@ -0,0 +1,10 @@
+#include "config.hpp"
+
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cout << argv[0] << " Version " << ChatApp_VERSION_MAJOR << "."
+                  << ChatApp_VERSION_MINOR << std::endl;
+    }
+}
