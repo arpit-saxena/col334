8fb9c59a015d4c013c7624b04c5f8b7651a7bee9
Arpit Saxena <arpit.saxena2000@yahoo.in>
2021-09-12 12:10:06 +0530

    Add clang format and format everything with it

--- a2/src/client.cpp
+++ a2/src/client.cpp
@@ -1,10 +1,10 @@
-#include "config.hpp"
-
 #include <iostream>
 
+#include "config.hpp"
+
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        std::cout << argv[0] << " Version " << ChatApp_VERSION_MAJOR << "."
-                  << ChatApp_VERSION_MINOR << std::endl;
-    }
+  if (argc < 2) {
+    std::cout << argv[0] << " Version " << ChatApp_VERSION_MAJOR << "."
+              << ChatApp_VERSION_MINOR << std::endl;
+  }
 }
